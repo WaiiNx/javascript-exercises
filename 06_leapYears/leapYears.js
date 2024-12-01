874531ecb28e42a961be4880bf06db03f3881bb0
WaiiNx <kevinsiebert02@gmail.com>
2024-12-01 22:43:34 +0100

    Finish Exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let divideBy4 = year%4===0
+    let divideBy100 = year%100===0
+    let divideBy400 = year%400===0
+    if (divideBy4 && (!divideBy100||divideBy400)){ 
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
