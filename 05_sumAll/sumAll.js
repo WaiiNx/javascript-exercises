fa06fb266f5783f8d9e7a949332a2b73aa95dd08
WaiiNx <kevinsiebert02@gmail.com>
2024-12-01 22:32:25 +0100

    Finish Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if (number1 < 0 || number2 < 0 || Number.isInteger(number1) == false || Number.isInteger(number2) == false) return "ERROR"
+    let result = 0
+    if (number1<number2) {
+    for (let i = number1; i<=number2;i++){
+        result = i+result;
+    }
+} else{
+    for (let i = number2; i<=number1;i++){
+        result = i+result;
+    }
+}
+    return result
 };
 
 // Do not edit below this line
