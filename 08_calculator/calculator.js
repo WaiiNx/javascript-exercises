fdba2f5768958829cbb1850a4e46cca0a0fde8bd
WaiiNx <kevinsiebert02@gmail.com>
2024-12-09 20:13:15 +0100

    Finish Exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	let result = number1 + number2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+  let result = number1 - number2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = array.reduce((a,b) => a + b, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let result = array.reduce((a,b) => a * b, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+  let result = number1 ** number2;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number1) {
+  let result = 1;
+  if (number1 === 0){
+    return 1;
+  }
+	for (let i = 2; i<=number1;i++)
+  {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
