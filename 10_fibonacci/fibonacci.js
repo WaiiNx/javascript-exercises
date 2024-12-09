e3c8bfe139fae57ebb614e67057af08b6d9f1d70
WaiiNx <kevinsiebert02@gmail.com>
2024-12-09 20:34:42 +0100

    Finish Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number1) {
+    let referenceNumber = parseInt(number1);
+    if (referenceNumber < 0) return "OOPS";
+    if (referenceNumber === 1) return 1;
+    let firstNumber = 0, secondNumber = 1; result = 0;
+    for (let i = 1; i < referenceNumber; i++){
+        result = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
