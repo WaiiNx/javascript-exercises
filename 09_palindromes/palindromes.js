06aaf318aaba9dd61d25c64a56570cde06e5e1c5
WaiiNx <kevinsiebert02@gmail.com>
2024-12-09 20:23:44 +0100

    Finish Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // remove unwated character
+    var re = /[\W_]/g;
+    // turn string to lowercase
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    // split the string, reverse it, and join it again
+    var reverseStr = lowRegStr.split('').reverse().join('');
+    // check if the reversed string is the same as the lowercase string 
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
